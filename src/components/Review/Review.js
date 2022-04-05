3a15d972fa1e5bd49a24131bb337f8199b80dd2b
Jager0002 <faisalmoin8991@gmail.com>
2022-04-05 22:34:17 +0600

    details added

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -11,8 +11,10 @@ const Review = ({ review }) => {
         <h2 className="font-semibold my-4">{name}</h2>
         <h2>Rating: {ratings}</h2>
         <h2 className="text-2xl my-4">{title}</h2>
-        <h2>{details.length > 200 ? details.slice(0, 200) : details}</h2>
-        <span className="font-semibold">...read more</span>
+        <h2>
+          {details.length > 200 ? details.slice(0, 200) : details}
+          <span className="font-semibold">...read more</span>
+        </h2>
       </div>
     </div>
   );
