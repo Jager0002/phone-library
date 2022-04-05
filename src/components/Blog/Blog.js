d8ec2e2c13ced0391140a5ae0b398ea7f0b53e6e
Jager0002 <faisalmoin8991@gmail.com>
2022-04-05 22:32:40 +0600

    styles added

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -2,16 +2,18 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div>
-      <h2 className="font-semibold">What is Semantic Tag?</h2>
+    <div className="mx-6 my-4">
+      <h2 className="text-center font-semibold">What is Semantic Tag?</h2>
       <h2>
         Semantic tags are basically meaningful tags that has uses as the name
         suggests, like article, nav, section etc. This type of tags are used to
         have mening both to browser and developer.
       </h2>
 
-      <h2 className="font-semibold">Inline, inline-block, block?</h2>
-      <h2 className="underline">Inline:</h2>
+      <h2 className="text-center font-semibold">
+        Inline, inline-block, block?
+      </h2>
+      <h2 className="underline font-semibold">Inline:</h2>
       <h2>
         Inline elements are used in a fragment, and it takes the properties of
         the upper elements. So, inline elements does not start a new line and
@@ -19,12 +21,12 @@ const Blog = () => {
         width cannot be set manually. for example: span, a ,img are inline
         elements.
       </h2>
-      <h2 className="underline">inline-block:</h2>
+      <h2 className=" underline font-semibold">inline-block:</h2>
       <h2>
         If we display inline-block the properties will be changed for instance
         we can fix height and width.
       </h2>
-      <h2 className="underline">block</h2>
+      <h2 className="underline font-semibold">block</h2>
       <h2>
         Block element appears in different lines and will take full width, and
         also the heights and width can be altered.
